15d9e04455aff6f769e93348648540dfc2e97461
CupcakePack <owji359@gmail.com>
2026-09-22 10:07:06 +1000

    Big Split (RICBM IV)
    Splits upon 4 in the first 100 rounds, splits upon 6 in the next 250 rounds, then never splits in the last 150 rounds.

---
+++ ricbm/main.py
@@ -0,0 +1,62 @@
+import helper as unswbc
+from helper import Direction, EdgeType
+import random
+
+ct: unswbc.Controller
+game: unswbc.Game
+
+# Seed so we get the same random generator every time.
+random.seed(0)
+
+def execute_turn() -> None:
+    # width, height = game.get_map_size()
+    if game.get_round_num() < 100 and ct.get_length() >= 4 and ct.get_unit_count() < 64:
+        child_size = ct.get_length() // 2
+        if ct.can_split(child_size):
+            ct.do_split(child_size)
+            return
+    elif game.get_round_num() < 350 and ct.get_length() >= 6 and ct.get_unit_count() < 64:
+        child_size = ct.get_lenght() // 2
+        if ct.can_split(child_size):
+            ct.do_split(child_size)
+            return
+    """Step onto the first open neighbouring tile."""
+    here = ct.get_position()
+    here_tile = ct.get_tile(here)
+
+    directions = Direction.get_direction_list()
+    random.shuffle(directions)
+    # ct.output_log(*directions)
+
+    for direction in directions:
+        
+        edge = here_tile.get_edge(direction).get_edge_type()
+        if edge == EdgeType.KELP:
+            continue
+
+        ahead = ct.get_tile(here.add_dir(direction))
+        if ahead.get_dragon() is not None:
+            continue
+        if ahead.has_pearl():
+            ct.make_move(direction)
+            return
+        # ct.output_log("Moving in", direction)
+        ct.make_move(direction)
+        return
+    
+    if ct.get_length() >= 4 and ct.can_split(ct.get_length() // 2):
+        ct.do_split(ct.get_length() // 2)
+        return
+
+    ct.make_move(Direction.NORTH)
+
+def main() -> None:
+    global ct, game
+    ct, game = unswbc.init()
+
+    while unswbc.update(ct, game):
+        execute_turn()
+        unswbc.end_turn()
+
+if __name__ == "__main__":
+    main()
